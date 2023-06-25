db96b9fca6f932ea77e5caf3bd70ef5904ce7525
Aneuris99 <136648820+Aneuris99@users.noreply.github.com>
2023-06-25 00:41:14 -0400

    Revert "MenuEmpleados.cs"
    This reverts commit 22ab4eed7e49eb7ece7a3c5695f40b85178085f9.

--- Program.cs
+++ Program.cs
@@ -10,70 +10,7 @@
     public decimal SueldoNeto { get; set; }
     public decimal TSS { get; set; }
     public decimal ImpuestoRenta { get; set; }
-class Program
-{
-    static Empleado[] empleados = new Empleado[100];
-    static int contadorEmpleados = 0;
 
-    static void Main(string[] args)
-    {
-        bool salir = false;
-
-        while (!salir)
-        {
-            Console.WriteLine("=== Sistema de Nómina ===");
-            Console.WriteLine("1. Agregar empleado");
-            Console.WriteLine("2. Ver empleados");
-            Console.WriteLine("3. Eliminar empleado");
-            Console.WriteLine("4. Ver nómina");
-            Console.WriteLine("5. Reporte de empleados mujeres");
-            Console.WriteLine("6. Reporte de empleados con licencia");
-            Console.WriteLine("7. Reporte de empleados con sueldo por encima de $50,000");
-            Console.WriteLine("8. Salir");
-            Console.Write("Seleccione una opción: ");
-            string? opcionStr = Console.ReadLine();
-
-            if (int.TryParse(opcionStr, out int opcion))
-            {
-                switch (opcion)
-                {
-                    case 1:
-                        AgregarEmpleado();
-                        break;
-                    case 2:
-                        VerEmpleados();
-                        break;
-                    case 3:
-                        EliminarEmpleado();
-                        break;
-                    case 4:
-                        VerNomina();
-                        break;
-                    case 5:
-                        ReporteEmpleadosMujeres();
-                        break;
-                    case 6:
-                        ReporteEmpleadosLicencia();
-                        break;
-                    case 7:
-                        ReporteSueldoSuperior();
-                        break;
-                    case 8:
-                        salir = true;
-                        break;
-                    default:
-                        Console.WriteLine("Opción inválida. Por favor, seleccione una opción válida.");
-                        break;
-                }
-            }
-            else
-            {
-                Console.WriteLine("Opción inválida. Por favor, seleccione una opción válida.");
-            }
-
-            Console.WriteLine();
-        }
-    }
     public void CalcularTSS()
     {
         decimal tasaTSS = 0.0304m; // Tasa de TSS (3.04%)
